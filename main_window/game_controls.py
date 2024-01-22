f023f396a9bc01fcf985a18d132afccd739a32fb
Ana Monici <ana.monici@outlook.com>
2024-01-22 18:15:59 -0300

    feat/buttons: Added buttons to the game status and game settings sections.

--- main_window/game_controls.py
+++ main_window/game_controls.py
@@ -3,8 +3,10 @@ Section of the main window where the
 game's status controls will be displayed.
 """
 
-from PyQt6.QtWidgets import QLabel, QWidget
-from PyQt6.QtGui import QPalette, QColor
+import os
+from PyQt6.QtWidgets import QLabel, QWidget, QPushButton, QHBoxLayout
+from PyQt6.QtGui import QPalette, QColor, QFont, QIcon
+from PyQt6.QtCore import QSize
 
 class GameControls(QWidget):
     def __init__(self):
@@ -15,4 +17,31 @@ class GameControls(QWidget):
         palette.setColor(QPalette.ColorRole.Window, QColor('#b3a4d3'))
         self.setPalette(palette)
 
-        QLabel("<h1> GameControls! </h1>", parent=self)
+        # Creating the PLAY, HALT and RESET buttons
+        # To add icon to the button we will use the QIcon object, which
+        # gets the path to an SVG image
+        path_to_icons = os.getcwd()+"/main_window/images/"
+
+        btn_start = QPushButton(icon=QIcon(path_to_icons+"start.svg"), text="START", parent=self)
+        btn_start.setIconSize(QSize(50, 50))
+        btn_start.setFont(QFont('Arial', 15))
+        btn_start.setFixedSize(160, 60)
+
+        btn_halt = QPushButton(icon=QIcon(path_to_icons+"halt.svg"), text="HALT", parent=self)
+        btn_halt.setIconSize(QSize(50, 50))
+        btn_halt.setFont(QFont('Arial', 15))
+        btn_halt.setFixedSize(160, 60)
+        
+        btn_reset = QPushButton(icon=QIcon(path_to_icons+"Reset_1.svg"), text="RESET", parent=self)
+        btn_reset.setIconSize(QSize(50, 50))
+        btn_reset.setFont(QFont('Arial', 15))
+        btn_reset.setFixedSize(160, 60)
+
+        # Adding bunttons to layout
+        layout = QHBoxLayout()
+        layout.addWidget(btn_start)
+        layout.addWidget(btn_halt)
+        layout.addWidget(btn_reset)
+        self.setLayout(layout)
+
+        # QLabel("<h1> GameControls! </h1>", parent=self)
--- main_window/game_settings.py
+++ main_window/game_settings.py
@@ -3,8 +3,10 @@ Section of the main window where the
 game's setting buttons will be displayed.
 """
 
-from PyQt6.QtWidgets import QLabel, QWidget
-from PyQt6.QtGui import QPalette, QColor
+import os
+from PyQt6.QtCore import QSize
+from PyQt6.QtWidgets import QLabel, QWidget, QVBoxLayout, QHBoxLayout, QPushButton
+from PyQt6.QtGui import QPalette, QColor, QFont, QIcon
 
 class GameSettings(QWidget):
     def __init__(self):
@@ -15,4 +17,63 @@ class GameSettings(QWidget):
         palette.setColor(QPalette.ColorRole.Window, QColor('#b3a4d3'))
         self.setPalette(palette)
 
-        QLabel("<h1> GameSettings! </h1>", parent=self)
+        self.current_color = 'blue'
+        self.current_side = 'left'
+
+        # Creating the change color and side buttons
+        self.path_to_icons = os.getcwd()+"/main_window/images/"
+        self.btn_change_color = QPushButton(
+            icon=QIcon(self.path_to_icons+"blue.svg"), text="Alternar Cor", parent=self
+        )
+        self.btn_change_color.setIconSize(QSize(40, 40))
+        self.btn_change_color.setFont(QFont('Arial', 15))
+        self.btn_change_color.setFixedSize(180, 60)
+        self.btn_change_color.clicked.connect(self.onClick)
+
+        self.btn_change_side = QPushButton(
+            icon=QIcon(self.path_to_icons+"esquerda.svg"), text="Alternar Lado", parent=self
+        )
+        self.btn_change_side.setIconSize(QSize(40, 40))
+        self.btn_change_side.setFont(QFont('Arial', 15))
+        self.btn_change_side.setFixedSize(180, 60)
+        self.btn_change_side.clicked.connect(self.onClick)
+
+        # Create game mode selection section
+        top_h_layout = QHBoxLayout()
+        top_h_layout.addWidget(QLabel("<h1> Modo treino / comp </h1>", parent=self))
+
+        # Adding buttons to layout
+        bottom_h_layout = QHBoxLayout()
+        bottom_h_layout.addWidget(self.btn_change_color)
+        # TODO alterar texto mostrando cor atual setIcon()
+        # bottom_h_layout.addWidget(QLabel("<h2> Atual: x </h2>", parent=self))
+        bottom_h_layout.addWidget(self.btn_change_side)
+        # TODO alterar texto mostrando lado atual
+        # bottom_h_layout.addWidget(QLabel("<h2> Atual: x </h2>", parent=self))
+
+        v_layout = QVBoxLayout()
+        v_layout.addLayout(top_h_layout)
+        v_layout.addLayout(bottom_h_layout)
+
+        self.setLayout(v_layout)
+
+        # QLabel("<h1> GameSettings! </h1>", parent=self)
+
+    def onClick(self):
+        sender = self.sender()
+        icon = sender.icon()
+
+        if sender is self.btn_change_color:
+            if self.current_color == 'blue':
+                sender.setIcon(QIcon(self.path_to_icons+"yellow.svg"))
+                self.current_color = 'yellow'
+            else:
+                sender.setIcon(QIcon(self.path_to_icons+"blue.svg"))
+                self.current_color = 'blue'
+        elif sender is self.btn_change_side:
+            if self.current_side == 'left':
+                sender.setIcon(QIcon(self.path_to_icons+"direita.svg"))
+                self.current_side = 'right'
+            else:
+                sender.setIcon(QIcon(self.path_to_icons+"esquerda.svg"))
+                self.current_side = 'left'
