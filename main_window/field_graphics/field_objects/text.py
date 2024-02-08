Fix: Rotation transformation at VertexShader.vsh and small fixes

main_window/field_graphics/field_objects/text.py
@@ -12,16 +12,18 @@ from OpenGL import GL
 class Text(Renderable):
     texture_id = -1
     texture_coordinates_VBO = -1
-    texture_coordinates_array = []
+    texture_coors = []
 
-    def __init__(self, display: str, texture_directory: str, color=None, size=1, fixed_rotation=False,
-                 tracking: Renderable | None = None):
+    def __init__(self, display: str, texture_directory: str, color=None, size=1, fixed_rotation=False, tracking: Renderable | None = None, anchor: tuple = (0, 0)):
 
         if color is None:
             color = [1, 1, 1, 1]
         self.display = display
         self.texture_directory = texture_directory
         self.color = color
+        self.tracking = tracking
+        self.fixed_rotation = fixed_rotation
+        self.anchor = anchor
 
         vertices = []
         colors = []
@@ -30,10 +32,10 @@ class Text(Renderable):
 
         for act in display:
             pos: int = ord(act) - 32
-            bmp_x: int = pos % 16;
+            bmp_x: int = pos % 16
             bmp_y: int = math.floor(pos / 16)
 
-            wspc_c = (i / 2 - display.__len__() / 4)
+            wspc_c = i / 2 - display.__len__() / 4
 
             vertices.append(wspc_c);      vertices.append(-.5); vertices.append(-.8)  # --
             vertices.append(wspc_c + .5); vertices.append(-.5); vertices.append(-.8)  # +-
@@ -43,38 +45,31 @@ class Text(Renderable):
             vertices.append(wspc_c + .5); vertices.append(-.5); vertices.append(-.8)  # +-
             vertices.append(wspc_c + .5); vertices.append(.5);  vertices.append(-.8)  # ++
 
+            # Sampla os as coordenadas da textura com base no sistema de coordenadas do OpenGL
+            # Assumindo que o bitmap seja de 16 * 16 caracteres, o que já é suficiente pro alfabeto ASCII
             txs_x_b = bmp_x / 16;       txs_y_e = 1 - (bmp_y / 16)
             txs_x_e = txs_x_b + 1 / 32; txs_y_b = txs_y_e - 1 / 16
 
-            self.texture_coordinates_array.append(txs_x_b)
-            self.texture_coordinates_array.append(txs_y_b)
+            self.texture_coors.append(txs_x_b); self.texture_coors.append(txs_y_b)
+            self.texture_coors.append(txs_x_e); self.texture_coors.append(txs_y_b)
+            self.texture_coors.append(txs_x_b); self.texture_coors.append(txs_y_e)
 
-            self.texture_coordinates_array.append(txs_x_e)
-            self.texture_coordinates_array.append(txs_y_b)
-
-            self.texture_coordinates_array.append(txs_x_b)
-            self.texture_coordinates_array.append(txs_y_e)
-
-            self.texture_coordinates_array.append(txs_x_b)
-            self.texture_coordinates_array.append(txs_y_e)
-
-            self.texture_coordinates_array.append(txs_x_e)
-            self.texture_coordinates_array.append(txs_y_b)
-
-            self.texture_coordinates_array.append(txs_x_e)
-            self.texture_coordinates_array.append(txs_y_e)
+            self.texture_coors.append(txs_x_b); self.texture_coors.append(txs_y_e)
+            self.texture_coors.append(txs_x_e); self.texture_coors.append(txs_y_b)
+            self.texture_coors.append(txs_x_e); self.texture_coors.append(txs_y_e)
 
             i += 1
 
-        self.texture_coordinates_array = np.asarray(self.texture_coordinates_array, dtype=numpy.float32)
+        self.texture_coors = np.asarray(self.texture_coors, dtype=numpy.float32)
 
-        for _ in vertices:
-            colors.append(0)
+        for i in range(int(vertices.__len__()/3)):
+            vertices[i*3] *= size; vertices[i*3+1] *= size
+            colors.append(0); colors.append(0); colors.append(0)
 
-        self.texture_id = loadTexture("main_window/field_graphics/assets/bitmaps/Impact.bmp")
+        self.texture_id = loadTexture(texture_directory)
         self.texture_coordinates_VBO = GL.glGenBuffers(1)
 
-        print(self.texture_coordinates_array)
+        print(self.texture_coors)
 
         vertices = np.asarray(vertices, dtype=np.float32)
         colors = np.asarray(colors, dtype=np.float32)
@@ -87,6 +82,12 @@ class Text(Renderable):
         super().__init__(vertices, colors, shader)
 
     def draw(self, tx, ty, scale, rotation, aspect_ratio, sim_time):
+
+        if self.tracking is None:
+            self.x = self.anchor[0]; self.y = self.anchor[1]
+        else:
+            self.x = self.anchor[0] + self.tracking.x; self.y = self.anchor[1] + self.tracking.y
+
         self.shaderProgram.bind()
         GL.glEnable(GL.GL_TEXTURE_2D)
         GL.glEnableVertexAttribArray(2)
@@ -104,4 +105,4 @@ class Text(Renderable):
     def update_vertex_attributes(self):
         super().update_vertex_attributes()
         GL.glBindBuffer(GL.GL_ARRAY_BUFFER, self.texture_coordinates_VBO)
-        GL.glBufferData(GL.GL_ARRAY_BUFFER, self.texture_coordinates_array, GL.GL_STATIC_DRAW)
+        GL.glBufferData(GL.GL_ARRAY_BUFFER, self.texture_coors, GL.GL_STATIC_DRAW)