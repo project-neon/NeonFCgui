64a34520f933e8b404770e125bdef0621f8e1364
Ana Monici <ana.monici@outlook.com>
2024-03-28 16:18:05 -0300

    fix/organization: organize GameMode class.

--- main_window/main_window.py
+++ main_window/main_window.py
@@ -8,7 +8,7 @@ import math
 import typing
 
 from PyQt6.QtWidgets import (
-    QMainWindow, QWidget, QRadioButton, QLabel,
+    QMainWindow, QWidget, QLabel,
     QVBoxLayout, QHBoxLayout, QGridLayout
 )
 from PyQt6.QtGui import QFont, QPalette, QColor
@@ -17,57 +17,6 @@ from PyQt6.QtCore import Qt, QTimerEvent
 from entities import Match
 from main_window.field_graphics.field_view import FieldView
 from main_window.widgets import *
-
-class GameMode(QWidget):
-
-    def __init__(self):
-        super(GameMode, self).__init__()
-        self.setAutoFillBackground(True)
-        palette = self.palette()
-        palette.setColor(QPalette.ColorRole.Window, QColor('#b3a4d3'))
-        self.setPalette(palette)
-
-        self.mode = 'training'
-
-        # Creating game mode 'checkboxes' (radio buttons)
-        self.btn_trainning = QRadioButton(text="Modo de treino", parent=self)
-        self.btn_trainning.toggled.connect(self.selectMode)
-        self.btn_trainning.setFont(QFont('Arial', 15))
-        self.btn_trainning.setStyleSheet("QRadioButton::font {spacing : 20px;}"
-            "QRadioButton::indicator"
-            "{"
-            "width : 20px;"
-            "height : 20px;"
-            "}"
-        )
-        
-        self.btn_competition = QRadioButton(text="Modo competição", parent=self)
-        self.btn_competition.toggled.connect(self.selectMode)
-        self.btn_competition.setFont(QFont('Arial', 15))
-        self.btn_competition.setStyleSheet("QRadioButton::indicator"
-            "{"
-            "width : 20px;"
-            "height : 20px;"
-            "}"
-        )
-
-        self.btn_trainning.setChecked(True)
-
-        mode_layout = QVBoxLayout()
-        mode_layout.addWidget(QLabel("Modo", parent=self), alignment=Qt.AlignmentFlag.AlignHCenter)
-        mode_layout.addWidget(self.btn_trainning)
-        mode_layout.addWidget(self.btn_competition)
-        self.setLayout(mode_layout)
-
-    def selectMode(self):
-        sender = self.sender()
-        if sender.isChecked():
-            if sender == self.btn_trainning:
-                self.mode = 'trainning'
-            elif sender == self.btn_competition:
-                self.mode = 'competition'
-            print("Mode: "+self.mode)
-            # TODO display this message on log?
 
 class MainWindow(QMainWindow):
 
@@ -101,22 +50,23 @@ class MainWindow(QMainWindow):
         self.log_widget = Log()
         
         # Adding game status controls widget
-        game_controls_widget = GameControls(self.context, self.log_widget)
+        self.game_controls_widget = GameControls(self.context, self.log_widget)
         h = int(self.window_height/10)
-        game_controls_widget.setFixedHeight(int(h*2.5))
-        top_h_layout.addWidget(game_controls_widget)
+        self.game_controls_widget.setFixedHeight(int(h*2.5))
+        top_h_layout.addWidget(self.game_controls_widget)
 
         # Adding game fouls section
-        fouls_widget = Fouls()
-        fouls_widget.setFixedHeight(int(h*2.5))
-        top_h_layout.addWidget(fouls_widget)
+        self.fouls_widget = Fouls()
+        self.fouls_widget.setFixedHeight(int(h*2.5))
+        top_h_layout.addWidget(self.fouls_widget)
 
         window_layout.addLayout(top_h_layout)
 
         # Lower section with field visualization,
         # robot informations, game informations and fouls
         bottom_h_layout = QHBoxLayout()
-        bottom_h_layout.addWidget(FieldView(self.context), stretch=1)
+        self.field_vis = FieldView(self.context)
+        bottom_h_layout.addWidget(self.field_vis, stretch=1)
 
         # GUI mode and NeonFC informations displayed
         # in a grid (10 rows, 6 columns)
@@ -124,22 +74,22 @@ class MainWindow(QMainWindow):
         grid = QGridLayout()
 
         # Widget to choose game mode
-        mode_widget = GameMode()
-        grid.addWidget(mode_widget, 0, 3, 1, 3) # starts at row:0, column:3, spans 1 row, spans 3 columns
+        self.mode_widget = GameMode()
+        grid.addWidget(self.mode_widget, 0, 3, 1, 3) # starts at row:0, column:3, spans 1 row, spans 3 columns
 
         # NeonFC's informations
-        game_info_widget = GameInfo()
-        grid.addWidget(game_info_widget, 1, 3, 4, 3)
-        self.updatable_components.append(game_info_widget)
+        self.game_info_widget = GameInfo()
+        grid.addWidget(self.game_info_widget, 1, 3, 4, 3)
+        self.updatable_components.append(self.game_info_widget)
 
         # Add log widget to grid
         grid.addWidget(self.log_widget, 5, 3, 5, 3)
 
         # Robots' informations section
         # TODO adjust for 6 robots when category == SSL
-        robots_widget = RobotsInfo()
-        grid.addWidget(robots_widget, 0, 0, 10, 3)
-        self.updatable_components.append(robots_widget)
+        self.robots_widget = RobotsInfo()
+        grid.addWidget(self.robots_widget, 0, 0, 10, 3)
+        self.updatable_components.append(self.robots_widget)
 
         # Adding grid on a widget for better control of its alignment
         grid_widget = QWidget()
--- main_window/widgets/__init__.py
+++ main_window/widgets/__init__.py
@@ -3,3 +3,4 @@ from main_window.widgets.fouls import Fouls
 from main_window.widgets.game_info import GameInfo
 from main_window.widgets.log import Log
 from main_window.widgets.robots_info import RobotsInfo
+from main_window.widgets.other_widgets import GameMode
