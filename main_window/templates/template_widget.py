7422137e794ca41366d81245984bd74040120e4e
Ana-Monici <ana.monici@outlook.com>
2025-01-21 12:33:35 -0300

    feat/templates: added ComboBox and CheckBox examples.

--- main_window/templates/template_widget.py
+++ main_window/templates/template_widget.py
@@ -108,11 +108,18 @@ class TemplateWindow(QWidget):
         self.grid_layout.addWidget(self.radio_btn_1, 1, 1) # starts at row:1, column:1
         self.grid_layout.addWidget(self.radio_btn_2, 1, 2) # starts at row:1, column:2
         self.grid_layout.addWidget(self.radio_btn_3, 1, 3) # starts at row:1, column:3
-        self.grid_layout.addWidget(self.radio_option_lbl, 2, 0, alignment=Qt.AlignmentFlag.AlignTop) # starts at row:2, column:0, aligned at the top of the grid cell
+        self.grid_layout.addWidget(self.radio_option_lbl, 2, 0, 1, 2, alignment=Qt.AlignmentFlag.AlignTop) # starts at row:2, column:0, spans 1 row, spans 2 columns, is aligned at the top of the grid cell
         # Checkboxes
-        # TODO
-        # Dropdown menu
-        # TODO
+        self.grid_layout.addWidget(self.checkbox_lbl, 3, 0) # starts at row:3, column:0
+        self.grid_layout.addWidget(self.checkbox_a, 3, 1) # starts at row:3, column:1
+        self.grid_layout.addWidget(self.checkbox_b, 3, 2) # starts at row:3, column:2
+        self.grid_layout.addWidget(self.checkbox_c, 3, 3) # starts at row:3, column:3
+        self.grid_layout.addWidget(self.checked_boxes_lbl, 4, 0, 1, 2, alignment=Qt.AlignmentFlag.AlignTop) # starts at row:4, column:0, spans 1 row, spans 2 columns, is aligned at the top of the grid cell
+        # Dropdown menu (Combo box)
+        self.grid_layout.addWidget(self.combobox_lbl, 5, 0) # starts at row:5, column:0
+        self.grid_layout.addWidget(self.combobox_btn, 5, 1, 1, 3) # starts at row:5, column:1, spans 1 row, spans 3 columns
+        self.grid_layout.addWidget(self.combobox_index_lbl, 6, 0, 1, 2) # starts at row:6, column:0, spans 1 row, spans 2 columns
+        self.grid_layout.addWidget(self.combobox_text_lbl, 6, 2, 1, 2) # starts at row:6, column:2, spans 1 row, spans 2 columns
 
         # Add label and custom widgets to the vertical layout
         self.v_layout.addWidget(self.vertical_section_lbl)
@@ -146,21 +153,48 @@ class TemplateWindow(QWidget):
 
         # Create labels for the Radio Buttons (single option buttons)
         self.radio_buttons_lbl = QLabel("Radio Buttons: ")
-        self.radio_option_lbl = QLabel("Selected Option: None")
+        self.radio_option_lbl = QLabel("Radio Option: None")
         # Create 3 radio buttons
         self.radio_btn_1 = QRadioButton("Option 1")
         self.radio_btn_2 = QRadioButton("Option 2")
         self.radio_btn_3 = QRadioButton("Option 3")
-        # Connect this button to its function called "radio_button"
+        # Connect radio buttons to their function called "radio_button"
         self.radio_btn_1.toggled.connect(lambda: self.radio_button(1))
         self.radio_btn_2.toggled.connect(lambda: self.radio_button(2))
         self.radio_btn_3.toggled.connect(lambda: self.radio_button(3))
 
         # Create checkbox labels and buttons
-        # TODO
+        self.checkbox_lbl = QLabel("Checkboxes: ")
+        self.checked_boxes_lbl = QLabel("Boxes checked: ")
+        # Create 3 checkboxes
+        self.checkbox_a = QCheckBox(text="A")
+        self.checkbox_b = QCheckBox(text="B")
+        self.checkbox_c = QCheckBox(text="C")
+        # Connect checkboxes to their function called "checkboxes"
+        self.checkbox_a.stateChanged.connect(self.checkboxes)
+        self.checkbox_b.stateChanged.connect(self.checkboxes)
+        self.checkbox_c.stateChanged.connect(self.checkboxes)
 
-        # Create dropdown menu labels and buttons
-        # TODO
+        # Create dropdown menu (Combo box) labels and buttons
+        self.combobox_lbl = QLabel("ComboBox: ")
+        self.combobox_index_lbl = QLabel("ComboBox index: ")
+        self.combobox_text_lbl = QLabel("ComboBox text: ")
+        # Create combo box and add options to it
+        self.combobox_btn = QComboBox()
+        # Adding items to the combobox
+        self.combobox_btn.addItems(['One', 'Two', 'Six'])
+        self.combobox_btn.addItem('Seven')
+        self.combobox_btn.insertItem(2, 'Three')
+        self.combobox_btn.insertItems(3, ['Four', 'Five'])
+        # Note: An item can be removed with removeItem(int index) and all items can be removed with clear()
+
+        # Connect combo box to its functions
+        self.combobox_btn.activated.connect(self.combobox_index)
+        self.combobox_btn.currentTextChanged.connect(self.combobox_text)
+        # self.combobox_btn.currentIndexChanged.connect(self.combobox_index)
+        # Note: currentIndexChanged() and currentTextChanged() are always emitted regardless
+        # if the change was done programmatically or by user interaction, while activated()
+        # is only emitted when the change is caused by user interaction.
     
     """
     # ██████╗░██╗░░░██╗████████╗████████╗░█████╗░███╗░░██╗██╗░██████╗
@@ -185,7 +219,33 @@ class TemplateWindow(QWidget):
     
     def radio_button(self, option):
         if option:
-            self.radio_option_lbl.setText(f"Selected Option: {option}")
+            self.radio_option_lbl.setText(f"Radio Option: {option}")
+    
+    def checkboxes(self):
+        text = "Boxes checked:"
+        if self.checkbox_a.isChecked():
+            text = text + " A"
+        if self.checkbox_b.isChecked():
+            text = text + " B"
+        if self.checkbox_c.isChecked():
+            text = text + " C"
+        self.checked_boxes_lbl.setText(text)
+    
+    def combobox_index(self, index):
+        # This function is called every time a combobox option is selected
+        text = "ComboBox index: " + str(index)
+        self.combobox_index_lbl.setText(text)
+        # The selected combobox option's text can be obtained as follows:
+        text2 = str(self.combobox_btn.currentText())
+        self.combobox_text_lbl.setText("ComboBox text: " + text2)
+        print("ComboBox")
+    
+    def combobox_text(self, text):
+        # This function is only called when the text in the combobox changes
+        self.combobox_text_lbl.setText("ComboBox text: " + text)
+        print(text)
+        # Alternatively, the selected combobox option's text can be obtained as follows:
+        # print(str(self.combobox_btn.currentText()))
 
     """
     # ░█████╗░██╗░░░██╗░██████╗████████╗░█████╗░███╗░░░███╗
