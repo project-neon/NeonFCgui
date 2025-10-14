fb1e28582543abf5280626dc04d660b117ffc70e
Eduardo Makita <makitayukio1@gmail.com>
2025-10-14 18:18:23 -0300

    fix: add percentage sign and fix rssi

--- entities/robot.py
+++ entities/robot.py
@@ -19,15 +19,5 @@ class Robot:
     def update_information(self, info, team=None):
         """ Function to update values received in api """
         self.robot_pos = (info.pos.x, info.pos.y, info.pos.z)
-
-        # if str(self.robot_id) in info['ROBOT_POS'].keys():
-        #     self.playing = True
-        #     self.robot_pos = info.get('ROBOT_POS', {}).get(str(self.robot_id), self.robot_pos)
-        #     if team:
-        #             self.strategy = info.get('STRATEGY', {}).get(str(self.robot_id), self.strategy)
-        #             self.battery = info.get('BATTERY', {}).get(str(self.robot_id), self.battery)
-        #             self.signal = info.get('SIGNAL', {}).get(str(self.robot_id), self.signal)
-        #             # self.kicker = info.get('KICKER', {}).get(str(self.robot_id), self.kicker)
-        # else:
-        #     self.playing = False
-        #     self.robot_pos = (-5,-5,0)
+        self.battery = f"{info.battery}%"
+        self.signal = info.rssi
